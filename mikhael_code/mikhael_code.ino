972e62a6158a07cec753451cc3ebc3ad3df4cf94
Mikhael Jude M. Dionio <mikhaeldionio@gmail.com>
2018-06-19 18:32:43 +0800

    initial code and start button debouncing
    add initial code
    copy debouncing and 7seg output functions
    add debouncing for start

--- mikhael_code/mikhael_code.ino
+++ mikhael_code/mikhael_code.ino
@@ -1,9 +1,83 @@
-void setup() {
-  // put your setup code here, to run once:
 
+//PIN DEFINITIONS
+#define startSwitch 4
+#define clockSwitch 5
+#define readyLED A0
+#define dataHazardLED A1
+#define controlHazardLED A2
+#define invalidExceptionLED A3
+#define arithmeticExceptionLED A4
+
+//GLOBAL VARIABLES
+bool hasStarted;
+long timeLastClockPress;
+int lastClockRead;
+long timeLastStartPress;
+int lastStartRead;
+int f;
+
+void setup() {
+
+  //PIN MODES
+  int i;
+  pinMode(startSwitch, INPUT);
+  pinMode(clockSwitch, INPUT);
+  for(i=6;i<14;i++){
+    pinMode(i, OUTPUT);
+  }
+  pinMode(readyLED, OUTPUT);
+  pinMode(dataHazardLED, OUTPUT);
+  pinMode(controlHazardLED, OUTPUT);
+  pinMode(invalidExceptionLED, OUTPUT);
+  pinMode(arithmeticExceptionLED, OUTPUT);
+
+  //SERIAL BEGIN
+  Serial.begin(9600);
+
+  char* p = "00fff";
+  f = (int)strtol(p, 0, 16);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(!hasStarted){
+    if(digitalRead(startSwitch) == HIGH){
+      debounceStartPoll();
+    }
+  }else{
+    digitalWrite(readyLED, HIGH);
+    Serial.println(f);
+  }
+}
 
+void outputBCD(int num, int pin0){
+  int i;
+  for(i = 0; i < 4; i++){
+    digitalWrite(pin0+i, num%2);
+    num /= 2;
+  }
+}
+
+void debounceClockPoll(){
+  int threshold = 30;
+  int clockRead = digitalRead(clockSwitch);
+  if(clockRead == HIGH && lastClockRead == LOW){
+    timeLastClockPress = millis();
+  }
+  if(millis()-timeLastClockPress > threshold && clockRead == HIGH){
+    //do action
+  }  
+  lastClockRead = clockRead;
+}
+
+void debounceStartPoll(){
+  int threshold = 30;
+  int startRead = digitalRead(clockSwitch);
+  if(startRead == HIGH && lastStartRead == LOW){
+    timeLastStartPress = millis();
+  }
+  if(millis()-timeLastStartPress > threshold && startRead == HIGH){
+    hasStarted = true;
+    Serial.print("START\0");
+  }  
+  lastStartRead = startRead;
 }
