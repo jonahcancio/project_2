540c79df2b51c3466974a494bef62094ed134890
Jonathan Cancio <jonahcancio@gmail.com>
2018-06-21 00:30:32 +0800

    made sense of readString, strtol and sprintf

--- jandel_code/jandel_code.ino
+++ jandel_code/jandel_code.ino
@@ -1,14 +1,37 @@
 #define CLOCK_PIN A0
 
-void setup() {
-  // put your setup code here, to run once:
 
+bool isPendingInput;
+char input[20];
+char output[20];
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("STARTO");
+//  memset(input, 0, 20);
+//  memset(output, 0, 20);
+  isPendingInput = true;
+  Serial.setTimeout(200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(isPendingInput){
+    if(Serial.available()){
+      Serial.readStringUntil(0).toCharArray(input, 20);
+      //parse input here
+      isPendingInput = false;
+    }
+  }else{
+    long decimal = strtol(input, NULL, 16);
+    Serial.println(decimal);
+    sprintf(output, "0x%08x", decimal);
+    Serial.println(output);
+    //reverse-parse output here
+    isPendingInput = true;
+  }
 }
+
+
 
 void outputBCD(int num, int pin0){
   int i;
