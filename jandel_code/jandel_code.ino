a729b1e992c485d37aea5f58ac17e2d39f7797c8
jonahcancio <jonahcancio@gmail.com>
2018-07-03 22:06:09 +0800

    got the r-types working

--- jandel_code/jandel_code.ino
+++ jandel_code/jandel_code.ino
@@ -14,9 +14,7 @@
 //Loop Boolean Hooks
 bool hasStartRequested;
 bool hasStartCommenced;
-bool cycle;
-bool start;
-bool pendingInput;
+bool isPendingInput;
 
 //Debouncing Hooks
 long timeLastClockPress;
@@ -27,39 +25,31 @@ int lastClockRead;
 char stringFromServer[MAX_STRLEN];
 char stringToServer[MAX_STRLEN];
 
-//separate strings from server
-char pFromServer[MAX_SUBLEN];
-char iFromServer[MAX_SUBLEN];
-char aFromServer[MAX_SUBLEN];
-char bFromServer[MAX_SUBLEN];
-char cFromServer[MAX_SUBLEN];
-
 //num variables to server
 long pNewPcInput;
 int aReadReg1Input;
 int bReadReg2Input;
 int cWriteRegInput;
-int dWriteDataReg;
+long dWriteDataReg;
 int eRegWrite;
-int fAddressMem;
-int gWriteDataMem;
+long fAddressMem;
+long gWriteDataMem;
 int hMemRead;
 int iMemWrite; 
 
 //variables from server
-int iNewInstruction;
-int aReadDataReg1Output;
-int bReadDataReg2Output;
-int cReadDataMemOutput;
+long iNewInstruction;
+long aReadDataReg1Output;
+long bReadDataReg2Output;
+long cReadDataMemOutput;
 
-//pipeline instruction queue
-int iPipelineQueue[5];
+//pipeline arrays
+long iPipelineQueue[5];
+long aluResultQueue[5];
+long writeBackResult;
 
-//processed variables
-char operation[4];
-int aluResultEx;
-int aluResultMem;
-int writeBackResult;
+//mutable strings for function returns
+char iString[MAX_SUBLEN] ;
 
 void setup() {
   //PIN MODES
@@ -81,9 +71,7 @@ void setup() {
   
   //BOOLEANS
   hasStartRequested = false;
-  hasStartCommenced = false;
-  cycle = false;
-  pendingInput = true;
+  hasStartCommenced = false;  isPendingInput = true;
 
   //OTHER SETUP
   memset(stringFromServer, 0, MAX_STRLEN);
@@ -96,22 +84,37 @@ void loop() {
     //debounceStartPoll();
     hasStartRequested = true;
     Serial.print("START");
-    Serial.write(0);
+    Serial.write(TERMINATOR);
   }else if(!hasStartCommenced){
     if(Serial.available() > 0){
-      Serial.readStringUntil(0).toCharArray(stringFromServer, MAX_STRLEN);
-      hasStartCommenced = true;
+      Serial.readStringUntil(TERMINATOR).toCharArray(stringFromServer, MAX_STRLEN);      
       initVars();
-      printToServerVars();
+      printPipelineInput();
+      hasStartCommenced = true;
+      isPendingInput = true;
+    }
+  }else if(isPendingInput){
+    if(Serial.available() > 0){
+      instIdentify(iPipelineQueue[2]);
+      Serial.print("Instruction for ALU: ");
+      Serial.println(iString);
+      aluResultQueue[2] = aluExecute(iString);
+      Serial.print("ALU RESULT: ");
+      Serial.println(aluResultQueue[2]);
+      Serial.readStringUntil(TERMINATOR).toCharArray(stringFromServer, MAX_STRLEN); 
+      storeServerOutput();         
+      isPendingInput = false;
     }
   }else{
-//    debounceClockPoll();
+    updatePipelineInput();
+    printPipelineInput();
+    isPendingInput = true;
   }
 }
 
 void initVars(){
   //init to server vars
-  pNewPcInput = strtoul(stringFromServer, NULL, 16);
+  pNewPcInput = strtoul(stringFromServer, NULL, 16)+4;
   aReadReg1Input = 0;
   bReadReg2Input = 0;
   cWriteRegInput = 0;
@@ -128,20 +131,10 @@ void initVars(){
   bReadDataReg2Output = 0;
   cReadDataMemOutput = 0;
   memset(iPipelineQueue, 0, 5);
+  memset(aluResultQueue, 0, 5);
 }
 
-void printToServerVars(){
-    sprintf(stringToServer, "%08x ", pNewPcInput);
-    Serial.print(stringToServer);
-    print5BitBinary(aReadReg1Input);
-    print5BitBinary(bReadReg2Input);
-    print5BitBinary(cWriteRegInput);
-    sprintf(stringToServer, "%08x %1d %08x %08x %1d %1d",
-      dWriteDataReg, eRegWrite, fAddressMem, gWriteDataMem, hMemRead, iMemWrite);
-    Serial.print(stringToServer);
-    Serial.write(TERMINATOR);
-}
-
+//prints num into a binary string of length 5
 void print5BitBinary(int num){
   int mult = 1;
   int bcd = 0;
@@ -156,77 +149,142 @@ void print5BitBinary(int num){
   Serial.print(output);
 }
 
-void outputBCD(int num, int pin0){
-  int i;
-  for(i = 0; i < 4; i++){
-    digitalWrite(pin0+i, num%2);
-    num /= 2;
-  }
+//prints the pipeline variables as input to server
+void printPipelineInput(){
+    sprintf(stringToServer, "%08lx ", pNewPcInput);
+    Serial.print(stringToServer);
+    print5BitBinary(aReadReg1Input);
+    print5BitBinary(bReadReg2Input);
+    print5BitBinary(cWriteRegInput);
+    sprintf(stringToServer, "%08lx %1d %08lx %08lx %1d %1d",
+      dWriteDataReg, eRegWrite, fAddressMem, gWriteDataMem, hMemRead, iMemWrite);
+    Serial.print(stringToServer);
+    Serial.write(TERMINATOR);
 }
 
-
-void serverOutputSegregate(char stringFromServer[MAX_STRLEN]){//Segregate instructions sent by the server
-  char *token;
-
+//parses stores the server input variables
+void storeServerOutput(){
+  char *token;  
   token = strtok(stringFromServer, " ");
-  strcpy(iFromServer,token);//IIIIIIII
+  iNewInstruction = strtoul(token, NULL, 16);//IIIIIIII  
+  token = strtok(NULL, " ");
+  aReadDataReg1Output = strtoul(token, NULL, 16);//AAAAAAA
+  token = strtok(NULL, " ");
+  bReadDataReg2Output = strtoul(token, NULL, 16);//BBBBBBBB
+  token = strtok(NULL, " ");
+  cReadDataMemOutput = strtoul(token, NULL, 16);//CCCCCCCC
+}
+
+
+
+//test with 01294820 00000007 00000003 00000005
+
+//Call every clock cycle to move pipeline and update pipeline variables to input to server
+void updatePipelineInput(){
+  char temp[MAX_SUBLEN];  
+  movePipelineQueue();
   
-  token = strtok(NULL, " ");
-  strcpy(aFromServer, token);//AAAAAAAA
+  pNewPcInput += 4;
+  
+  aReadReg1Input = (iPipelineQueue[1]>>21)&0x1f;
+  bReadReg2Input = (iPipelineQueue[1]>>16)&0x1f;
+  cWriteRegInput = (iPipelineQueue[1]>>11)&0x1f;
 
-  token = strtok(NULL, " ");
-  strcpy(bFromServer,token);//BBBBBBBB
+  instIdentify(iPipelineQueue[4]);
+  Serial.print("instruction at WB: ");
+  Serial.println(iString);
+  if(strcmp(temp, "lw")==0 || strcmp(temp, "nop")==0){
+    dWriteDataReg = cReadDataMemOutput;
+  }else{
+    dWriteDataReg = aluResultQueue[4];
+  }
+  eRegWrite = strcmp(temp, "j")!=0 && strcmp(temp, "beq")!=0 && strcmp(temp, "sw")!=0;
+  fAddressMem = aluResultQueue[3];
+  gWriteDataMem = iPipelineQueue[4]&0xffff;
 
-  token = strtok(NULL, " ");
-  strcpy(cFromServer,token);//CCCCCCCC
+  instIdentify(iPipelineQueue[3]);
+  Serial.print("instruction at MEM: ");
+  Serial.println(iString);
+  hMemRead = strcmp(temp, "lw") == 0;
+  iMemWrite = strcmp(temp, "sw") == 0;  
 }
 
-void serverOutputDecode(){
-  iNewInstruction = strtol(iFromServer, NULL, 16);
-  aReadDataReg1Output = strtol(aFromServer, NULL, 16);
-  bReadDataReg2Output = strtol(bFromServer, NULL, 16);
-  cReadDataMemOutput = strtol(cFromServer, NULL, 16);
-}
-
-int extractOpcode(){
-  long opcode;
-  opcode = iNewInstruction&4227858432;
-  opcode = opcode>>25;
-  return opcode;
-}
-
-void enqueuePipeline(){
+//move each instruction in the pipeline one stage to the right
+void movePipelineQueue(){
   int j;
   for(j = 4; j > 1; j--){
     iPipelineQueue[j] = iPipelineQueue[j-1];
   }
   iPipelineQueue[1] = iNewInstruction;
-  aluResultMem = aluResultEx;
-}
-
-void RType(long instruction){}
-
-
-void pipelineClockCycle(){
-  enqueuePipeline();
-  aReadReg1Input = (iPipelineQueue[1]>>21)&0x1f;
-  bReadReg2Input = (iPipelineQueue[1]>>16)&0x1f;
-  cWriteRegInput = (iPipelineQueue[1]>>11)&0x1f;
-  RType(iPipelineQueue[4]);
-  if(strcmp(operation, "lw")==0){
-    dWriteDataReg = cReadDataMemOutput;
-  }else{
-    dWriteDataReg = aluResultMem;
-  }
-  eRegWrite = strcmp(operation, "j")!=0 
-    && strcmp(operation, "beq")!=0
-    && strcmp(operation, "sw")!=0;
-  fAddressMem = aluResultEx;
-  gWriteDataMem = iPipelineQueue[4]
-  hMemRead = strcmp(operation, "lw") == 0;
-  iMemWrite = strcmp(operation, "sw") == 0;
   
+  for(j = 4; j > 2; j--){
+    aluResultQueue[j] = aluResultQueue[j-1];
+  }  
 }
+
+void instIdentify(long iCode){//identifies the specific operation  
+  long opCode;
+  long func;
+  opCode = (iCode>>26)&0x3f;
+  func = iCode & 0x3f;
+  
+  if(opCode == 0){//R-type
+    switch(func){//identify operation
+      case 0x20:{
+        strcpy(iString, "add");
+        break;
+      }
+      case 0x22:{
+        strcpy(iString, "sub");
+        break;
+      }case 0x24:{
+        strcpy(iString, "and");
+        break;
+      }case 0x25:{
+        strcpy(iString, "or");
+        break;
+      }case 0x2a:{
+        strcpy(iString, "slt");
+        break;
+      }
+      default:
+        strcpy(iString, "nop");
+    }
+  }else if(opCode == 2){//J-type
+  
+  }else{
+  }
+}
+
+long aluExecute(char *iString){//executes R-type instructions
+  long rs;
+  long rt;
+  long rd;
+  
+  rs = aReadDataReg1Output;
+  rt = bReadDataReg2Output;
+  Serial.print("rs: ");
+  Serial.print(rs);
+  Serial.print("    rt: ");
+  Serial.println(rt);
+  if(strcmp(iString, "add") == 0){
+    rd = rs + rt;
+  }else if(strcmp(iString, "sub") == 0){
+    rd = rs - rt;
+  }else if(strcmp(iString, "and") == 0){
+    rd = rs & rt;
+  }else if(strcmp(iString, "or") == 0){
+    rd = rs | rt;
+  }else if(strcmp(iString, "slt") == 0){
+    if(rs < rt){
+      rd = 1;
+    }else{
+      rd = 0;
+    }
+  }
+  return rd;
+}
+
 
 long timeLastStartPress;
 bool isDebouncing;
@@ -245,6 +303,15 @@ void debounceStartPoll(){
     Serial.print("START");
     Serial.write(0);
     digitalWrite(readyLED, HIGH);
+  }
+}
+
+
+void outputBCD(int num, int pin0){
+  int i;
+  for(i = 0; i < 4; i++){
+    digitalWrite(pin0+i, num%2);
+    num /= 2;
   }
 }
 
